651610732e97736e3f7888e5b40beab0045d2cca
Jagrut Sharma <jagrut.sharma1@gmail.com>
2022-08-22 22:54:12 +0530

    added transition affect

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const birthDate = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
-const checkBtn = document.querySelector(".btn");
+const checkBtn = document.querySelector(".link-btn");
 const result = document.querySelector(".result");
 const luckyImage = document.querySelector(".lucky");
 const unluckyImage = document.querySelector(".notlucky");
@@ -9,11 +9,13 @@ checkBtn.addEventListener("click", function () {
     if(Number(birthDate.value) === 0){
         messagePrint("Please enter date.");
         result.style.color = "#d97706";
+        unsetImage("none", "none");
     }
     
     else if (Number(luckyNumber.value) <= 0 ) {
         messagePrint("Please enter a value that is above zero.");
         result.style.color = "#d97706";
+        unsetImage("none", "none");
     }    
     
     else {
@@ -22,13 +24,12 @@ checkBtn.addEventListener("click", function () {
         if (sumOfDigits % Number(luckyNumber.value) === 0) {
             messagePrint("You are lucky! 🤗");
             result.style.color = "#15803d";
-            luckyImage.style.display = "block";
-            unluckyImage.style.display = "none";
+            unsetImage("none", "block");
         } else {
             messagePrint("You are not that lucky! 😥");
             result.style.color = "#1e40af";
-            luckyImage.style.display = "none";
-            unluckyImage.style.display = "block";
+            unsetImage("block", "none");
+
         }
     }
 });
@@ -46,4 +47,9 @@ function sumCalc(dateOfBirth) {
 
 function messagePrint(message) {
     result.innerText = message;
+}
+
+function unsetImage(state1, state2){
+    unluckyImage.style.display = state1;
+    luckyImage.style.display = state2;
 }
